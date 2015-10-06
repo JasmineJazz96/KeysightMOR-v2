d97a1c05e09980cd5782ef544f333a87eeea512c
Adrian Khor <adriank96@outlook.com>
2015-10-06 11:11:02 +0800

    Oct 5 Progress Update meeting build

---
+++ KeysightMOR-v2/Template.Master.cs
@@ -0,0 +1,64 @@
+﻿using KeysightMOR.Assets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls.WebParts;
+
+namespace KeysightMOR
+{
+    public partial class Template : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["username"] == null)
+            {
+                Response.Redirect("~/SignIn.aspx");
+            }
+            //-----------------------------------------------
+            lblCopyrightMessage.Text = Shared.CopyrightMessage;
+            if (Session["username"] != null)
+            {
+                welcome_msg.Text = "Welcome, " + Session["username"].ToString();
+            }
+            if (Request.FilePath.ToString().Contains("ControlPanel"))
+            {
+                navbar_admin.Attributes["class"] = "active";
+            }
+            else if (Request.FilePath.ToString().Contains("ReportView"))
+            {
+                navbar_report.Attributes["class"] = "active";
+            }
+            else if (Request.FilePath.ToString().Contains("DataEntry"))
+            {
+                navbar_evaluate.Attributes["class"] = "active";
+            }
+            else if (Request.FilePath.ToString().Contains("ProfileSetting"))
+            {
+                navbar_setting.Attributes["class"] = "active";
+
+            }
+
+        }
+
+        protected void Unnamed_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void hlLogOut_Click(object sender, EventArgs e)
+        {
+           
+            Session["username"] = null;
+            Session["userid"] = null;
+            Session["position"] = null;
+
+            Response.Redirect("LogOut.aspx");
+        }
+    }
+}
